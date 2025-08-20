1f46068af6fa8a1318b55465f67b91323a2bdb90
ShifatHasanGNS <shifathasangns@gmail.com>
2025-08-20 16:47:26 +0600

    modified code after adding Class

--- calculator.cpp
+++ calculator.cpp
@@ -1,85 +1,89 @@
-// import necessary libraries
-#include <iostream>
 #include <cmath>
+#include <iostream>
 
-// use the namespace std
 using namespace std;
 
-// main function 
-int main()
-{
-    // put the program in a loop using the while statement
-    while (true)
-    {
-      // variables for numbers we're gonna calculate and also operation number
-      double num1, num2;
-      int operation;
+class Calculator {
+private:
+  int operation;
+  double num1;
+  double num2;
 
-      // first ask the user for operation number and show them a list of operations with their own unique number
-      cout << "\nPlease enter your operation from the list down below:\n";
-      cout << "1: Sum\n2: Substraction\n3: Multiplication\n4: Division\n5: Square Root\n\n6: EXIT" << endl;
+public:
+  Calculator() {}
 
-      // prompt the user for their operation number
-      cout << "-> ";
-      cin >> operation;
+  int get_operation() { return this->operation; }
 
-      // if operation is NOT 6, meaning the user is not willing to execute the 6th operation which basically exits the program
-      if (operation != 6) {
-        // prompt the user for the first number
-        cout << "\nPlease enter the first number.\n";
-        cout << "-> ";
-        cin >> num1;
+  double get_num1() { return num1; }
 
-        // if operation was 5 (square root) then calculate the square root of first entered number and output it to the console
-        if (operation == 5) {
-          cout << "\nResult is: " << sqrt(num1) << endl;
+  double get_num2() { return num2; }
+
+  void set_operation(int operation) { this->operation = operation; }
+
+  void set_num1(double num1) { this->num1 = num1; }
+
+  void set_num2(double num2) { this->num2 = num2; }
+
+  void calc_and_show() {
+    if (this->operation < 6) {
+      if (this->operation == 5) {
+        cout << "\nResult is: " << sqrt(num1) << endl;
+      } else {
+        switch (operation) {
+        case 1:
+          cout << "\nResult is: " << num1 + num2 << endl;
+          break;
+
+        case 2:
+          cout << "\nResult is: " << num1 - num2 << endl;
+          break;
+
+        case 3:
+          cout << "\nResult is: " << num1 * num2 << endl;
+          break;
+
+        case 4:
+          cout << "\nResult is: " << num1 / num2 << endl;
+          break;
+
+        default:
+          cout << "\nWrong operation.\n";
+          break;
         }
-        // otherwise, prompt the user for the second number
-        else{
-          cout << "\nPlease enter the second number.\n";
-          cout << "-> ";
-          cin >> num2;
-
-          // use a switch statement instead of a million if-statements. this gives us mamy advantages, mostly in terms of performance
-          switch (operation)
-          {
-
-              // if the operation number was 1, then calculate the sum of both first and second numbers
-              case 1:
-                  cout << "\nResult is: " << num1 + num2 << endl;
-                  break;
-    
-              // if the operation number was 2, then calculate the substraction of both first and second numbers
-              case 2:
-                  cout << "\nResult is: " << num1 - num2 << endl;
-                  break;
-    
-              // if the operation number was 3, then calculate the multiplication of both first and second numbers
-              case 3:
-                  cout << "\nResult is: " << num1 * num2 << endl;
-                  break;
-
-              // if the operation number was 4, then calculate the division of both first and second numbers
-              case 4:
-                  cout << "\nResult is: " << num1 / num2 << endl;
-                  break;
-    
-              // if the operation number was none of the above numbers, meaning that the user has entered a wrong operation number then simply
-              // output an string saying that the entered operation is wrong
-              default:
-                  cout << "\nWrong operation.\n";
-                  break;
-                  // at this point, we've reached the end of the program. so what happens is that the program goes back to the
-                  // line 10 and again repeats the cycle. this cycle goes forever, until the user enters the operation number 6 (which closes the program)
-                  // or program gets closed by other factors, such as facing any sort of error or user killing the process
-            }
-          }
-        }
-      else{
-          // if program reaches this line of code, it means the user has entered the number "6" as operation number
-          // and so what we need to do is to break the loop and simply finish the program
-          return 0;
       }
-
+    } else {
+      cout << "\nWrong operation.\n";
     }
+  }
+};
+
+int main() {
+  Calculator calc;
+
+  while (true) {
+    double num1, num2;
+    int operation;
+
+    cout << "\nPlease enter your operation from the list down below:\n";
+    cout << "1: Sum\n2: Substraction\n3: Multiplication\n4: Division\n5: "
+            "Square Root\n\n6: EXIT"
+         << endl;
+
+    cout << "-> ";
+    cin >> operation;
+
+    cout << "Enter Num1: ";
+    cin >> num1;
+
+    cout << "Enter Num2: ";
+    cin >> num2;
+
+    calc.set_operation(operation);
+    calc.set_num1(num1);
+    calc.set_num2(num2);
+
+    calc.calc_and_show();
+  }
+
+  return 0;
 }
